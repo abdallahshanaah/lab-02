0455d0884ce6fcb9fef08afce99837d338aee211
abdallahshanaah <abdallah_m_sh@yahoo.com>
2020-04-12 21:31:36 +0300

    done

--- js/apps.js
+++ js/apps.js
@@ -1,69 +1,143 @@
-'use strict';
+`use strict`;
 
-let galeryArray = [];
 let keywordArray = [];
+let productArray = [];
 
 
-$.get('./data/page-1.json')
-  .then(data => {
-    data.forEach((element) => {
-
-      let newGallery = new Gallery(element);
-      newGallery.render();
-    });
-    keywordArray.forEach(val => {
-      theOption(val);
-    });
-
-  })
-  .then(() => selected());
-
+$(function () {
+  renderFirstTime('./data/page-1.json');
+  $('#page').on('click',function(event){
+    let userClick = event.target.id;
+    if(userClick === 'pageOne'){
+      renderFirstTime('./data/page-1.json');
+    }
+    else if(userClick === 'pageTwo'){
+      renderFirstTime('./data/page-2.json');
+    }
+  });
+});
 
 
-function Gallery(val) {
-  this.image_url = val.image_url;
+
+function renderFirstTime(urlPath) {
+  $('#filter').empty('');
+  $('#showImage').empty('');
+  $('#filter').append('<option>Filter By Keywords</option>');
+  $.ajax({
+    url: urlPath,
+    dataType: 'json',
+    success: (data => {
+      let product;
+      data.forEach(value => {
+        product = new Product(value);
+        product.render();
+      });
+      keywordArray.forEach(value => { product.renderOption(value); });
+      product.event1();
+      product.event2();
+    }),
+  });
+}
+function Product(val) {
+  this.filePath = val.image_url;
   this.title = val.title;
   this.description = val.description;
-  this.keyword = val.keyword;
+  this.keyWords = val.keyword;
   this.horns = val.horns;
-  galeryArray.push(this);
-  if (keywordArray.includes(this.keyword) === false) {
-    keywordArray.push(this.keyword);
+  productArray.push(this);
+  if (keywordArray.includes(this.keyWords) === false) {
+    keywordArray.push(this.keyWords);
   }
 }
 
-
-Gallery.prototype.render = function () {
-
-
-  let clone = $('.photo-template').clone();
-  clone.removeClass('photo-template');
-  clone.find('h2').text(this.title); 
-  clone.find('img').attr('src', this.image_url);
-  clone.find('p').text(this.description);
-  $('main').append(clone);
-
-
+Product.prototype.render = function () {
+  let template = $('#mustacheR').html();
+  let myHtml =  Mustache.render(template,this);
+  $('#showImage').append(myHtml);
 };
-function theOption(val) {
 
-  let options = $('<option></option>').text(val);
-  $('select').append(options);
-}
+Product.prototype.renderOption = function (i) {
+  let option = $('<option></option>').text(i);
+  $('#filter').append(option);
+};
+
+let filterArray = [];
+Product.prototype.event1 = function () {
+  $('#filter').change(function () {
+    let choose = $('#filter').val();
+    let select2 = productArray.filter((n) => n.keyWords === choose);
+    $('#showImage').empty('');
+    filterArray = select2;
+    select2.forEach(value => {
+      value.render();
+    }
+    );
+    if (choose === 'Filter By Keywords') {
+      filterArray = [];
+      productArray.forEach(val =>{
+        val.render();
+      } );
+    }
+  });
+};
+Product.prototype.event2 = function () {
+  $('#sort').change(function () {
+    if (filterArray.length !== 0){
+      forFilter(filterArray);
+
+    }
+    else {
+      forFilter(productArray);
+
+    }});
+};
+let count = 0;
 
 
-function selected() {
-  $('select').on('change', function () {
-
-    let selected = $(this).val();
-    console.log(selected);
-    let allselected = galeryArray.filter((element) => element.keyword === selected);
-    console.log(allselected);
-    $('section:not(:first)').remove();
-    allselected.forEach(value => {
+$('#showImage').on('click',(event) =>{
+  let clickEle = event.target.src;
+  let newClick = productArray.filter(n => n.filePath === clickEle);
+  $('#showImage').empty('');
+  newClick[0].render();
+  $('#showImage').addClass('bigger');
+  $('#showImage').removeAttr('id');
+  count ++;
+  if (count === 2){
+    count = 0;
+    $('.bigger').attr('id','showImage');
+    $('#showImage').empty('');
+    productArray.forEach(value => {
       value.render();
     });
+    $('#showImage').removeClass('bigger');
 
+  }
 
-  });
+});
+function forFilter(array) {
+  let choose = $('#sort').val();
+  if (choose === 'text') {
+    array.sort((a, b) => {
+      if (a.title < b.title) {
+        return -1;
+      }
+      else {
+        return 1;
+      }
+    });
+    $('#showImage div').hide();
+
+    array.forEach(val => {
+      val.render();
+    });
+  }
+  else if (choose === 'num') {
+    array.sort((a, b) => {
+      return a.horns - b.horns;
+    });
+    $('#showImage div').hide();
+    array.forEach(val => {
+      val.render();
+    });
+  }
 }
